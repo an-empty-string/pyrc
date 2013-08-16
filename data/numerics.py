9653782143468e4d6abe2f564631c0f9466e7199
Fox Wilson <foxwilson123@gmail.com>
2013-08-16 18:20:51 -0400

    gpl header

--- data/numerics.py
+++ data/numerics.py
@@ -1,3 +1,24 @@
+#     //   ) )          //   ) )  //   ) ) 
+#    //___/ /          //___/ /  //        
+#   / ____ / //   / / / ___ (   //         
+#  //       ((___/ / //   | |  //          
+# //            / / //    | | ((____/ /    
+#
+# This file is part of PyRC.
+#
+#    PyRC is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    PyRC is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with PyRC.  If not, see <http://www.gnu.org/licenses/>.
+#
 numerics = {
         "RPL_WELCOME": "001",
         "RPL_YOURHOST": "002",
